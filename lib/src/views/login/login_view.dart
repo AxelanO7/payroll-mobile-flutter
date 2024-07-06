f6bc644946311517db606543e682964c08586b4a
AxelanO7 <jeremia123.jm@gmail.com>
2024-07-07 01:49:41 +0800

    fix (login) : gesture detector for login input

--- lib/src/views/login/login_view.dart
+++ lib/src/views/login/login_view.dart
@@ -30,57 +30,62 @@ class LoginView extends StatelessWidget {
       mobile: (controller) => GetBuilder<LoginController>(
         builder: (controller) => PopScope(
           canPop: controller.canPop,
-          child: Scaffold(
-            backgroundColor: ColorStyle.whiteColor,
-            body: ScrollConfiguration(
-              behavior: CustomDisableGlowBehavior(),
-              child: SingleChildScrollView(
-                child: SizedBox(
-                  height: Get.height - 32,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/images/global/logo.png', width: Get.width / 2.5, height: Get.width / 2.5),
-                      const SizedBox(height: 32),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 36),
-                        child: TextField(
-                          controller: controller.usernameController,
-                          decoration: InputDecoration(
-                            hintText: 'Masukkan username',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
+          child: GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: Scaffold(
+              backgroundColor: ColorStyle.whiteColor,
+              body: ScrollConfiguration(
+                behavior: CustomDisableGlowBehavior(),
+                child: SingleChildScrollView(
+                  child: SizedBox(
+                    height: Get.height - 32,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/images/global/logo.png', width: Get.width / 2.5, height: Get.width / 2.5),
+                        const SizedBox(height: 32),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 36),
+                          child: Focus(
+                            child: TextField(
+                              controller: controller.usernameController,
+                              decoration: InputDecoration(
+                                hintText: 'Masukkan username',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 32),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 36),
-                        child: TextField(
-                          controller: controller.passwordController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            hintText: 'Masukkan password',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
+                        const SizedBox(height: 32),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 36),
+                          child: TextField(
+                            controller: controller.passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Masukkan password',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 32),
-                      CustomButton(
-                        isLoading: controller.isLoadingLogin,
-                        width: Get.width - 72,
-                        height: 44,
-                        color: ColorStyle.customGreyColor,
-                        onPressed: controller.handleLogin,
-                        child: Text(
-                          'Login',
-                          style: TypographyStyle.body2Bold.copyWith(color: ColorStyle.whiteColor),
+                        const SizedBox(height: 32),
+                        CustomButton(
+                          isLoading: controller.isLoadingLogin,
+                          width: Get.width - 72,
+                          height: 44,
+                          color: ColorStyle.customGreyColor,
+                          onPressed: controller.handleLogin,
+                          child: Text(
+                            'Login',
+                            style: TypographyStyle.body2Bold.copyWith(color: ColorStyle.whiteColor),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
